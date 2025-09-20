9da684ddf674383292ef4694bf1a4fc28878ecd1
Tracy Urquhart <tracy.allen.urquhart@gmail.com>
2025-09-20 16:12:01 -0400

    add merge sort

--- src/sorts.h
+++ src/sorts.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "sort_helper.h"
 
 /*** code for selection sort ****/
@@ -61,8 +62,7 @@ void selectionSortIntegers(int *array, unsigned int size, int print) {
 //  - 'size' tells us how big the array of data is we are sorting.
 //  - 'print' tells it to print out after each iteration 
 // Output: No value is returned, but 'array' should be modified to store a sorted array of numbers.
-void insertionSortIntegers(int *array, unsigned int size, int print)
-{
+void insertionSortIntegers(int *array, unsigned int size, int print) {
     for (int i = 0; i < size; i++) {
         int key = array[i];
         int j = i; 
@@ -91,8 +91,7 @@ void insertionSortIntegers(int *array, unsigned int size, int print)
 // param(3) 'print' tells it to print out after each iteration.
 // Output:   No value is returned, but 'array' should
 //           be modified to store a sorted array of size.
-void bubbleSortIntegers(int *array, unsigned int size, int print)
-{
+void bubbleSortIntegers(int *array, unsigned int size, int print) {
     for(int i = 0; i < size; i++) {
         bool swapped = false;
         for(int j = 0; j < size - i - 1; j++){
@@ -112,6 +111,20 @@ void bubbleSortIntegers(int *array, unsigned int size, int print)
 // Merges two subarrays of arr[].
 // First subarray is arr[l..m]
 // Second subarray is arr[m+1..r]
+
+// =============== Sort Function ===============
+// Name: bubblesort
+// param(1): 'arr' is a pointer to an integer address.
+//           This is the start of some 'contiguous block of memory'
+//           that we will sort.
+// param(2): 'temp' is a pointer to an integer address.
+//           This is the start of some 'contiguous block of memory'
+//           that we will use temporarily.
+// param(3)  'l' the start of the first array
+// param(4) 'm' the midpoint
+// param(5) 'r' the end of the second array
+// Output:   No value is returned, but 'arr' should
+//           be modified a sorted array.
 void merge(int arr[], int temp[], int l, int m, int r)
 {
     if (arr == NULL || temp == NULL)
@@ -122,7 +135,48 @@ void merge(int arr[], int temp[], int l, int m, int r)
     if (l > m || m + 1 > r)
         return;
 
+    int leftSize = m - l + 1; // the size of the left subarray
+    int rightSize = r - m; // the size of the right subarray
 
+    int *leftArray = temp; // point to beginning of left
+    int *rightArray = temp + leftSize; // uses pointer math to get beginning of R
+
+    // copy using memcpy based on video walkthru. this is more memory efficient 
+    // but accomplishes the same thing as looping through and copying
+    memcpy(leftArray, arr + l, sizeof(int) * leftSize);
+    memcpy(rightArray, arr + m + 1, sizeof(int) * rightSize);
+
+    // merge back together
+
+    int i = 0;    // initialize first subarray index to 0
+    int j = 0;    // initialize second subarray index to 0
+    int k = l;    // initialize merged subarray to start of left array
+
+    while (i < leftSize && j < rightSize) {
+        // if left less than or equal to right
+        // then copy left value into arr[k] and inc i
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            i++;
+        } else { // otherwise, copy the right value and inc j
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++; 
+    }
+
+    // if there are still elements in the left, copy into arr
+    while (i < leftSize) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+    // if there are still elements in the right, copy into arr
+    while (j < rightSize) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
 }
 
 // To be built during week 6 lab
@@ -134,9 +188,17 @@ void merge(int arr[], int temp[], int l, int m, int r)
 //          	This helps temporarily store the sorted subarray.
 //          (3) 'l' and 'r' are integers, which are the first index and the last index of 'arr' respectively.
 // Output: No value is returned, but 'array' should be modified to store a sorted array of numbers.
-void merge_sort(int arr[], int temp[], int l, int r)
-{
-   
+void merge_sort(int arr[], int temp[], int l, int r) {
+   if (l < r) {
+        // find the mid point
+        // we use this method instead of the more intuitive 
+        // m = l + r / 2 because it is more defensive and will 
+        // not result in overflow if the ints are large
+        int m = l + (r - l) / 2;
+        merge_sort(arr, temp, l, m);      // recursively sort through left half
+        merge_sort(arr, temp, m + 1, r);  // recursively sort through right half
+        merge(arr, temp, l, m, r); // merge sorted sections back together
+    }
 }
 
 // lab build, merge sort
